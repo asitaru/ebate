4bfa4f7d5fb83beceae2123da95d36e91edb986d
asitaru <andreisitaru93@gmail.com>
2017-04-17 22:19:52 +0300

    changed post route from async/await to promise for better functionaliy

--- app/controllers/post-controller.js
+++ app/controllers/post-controller.js
@@ -1,6 +1,6 @@
 let Ebate = require('../models/ebate');
 
-module.exports = async(req,res) => {
+module.exports = (req,res) => {
 
     //we prepare the new ebate to be inserted in db
     let ebate = new Ebate();
@@ -9,15 +9,15 @@ module.exports = async(req,res) => {
     req.body.options.forEach( option => ebate.options.push({name: option, votes: 0}));
     console.log(ebate);
 
-    try {
-        //try saving the ebate
-        await ebate.save();
-
-        //return a succesfull message upon insertion
-        res.json({ message: 'Ebate created!'});
-    }
-    catch(err) {
+    //try saving the ebate
+     ebate.save().then(
+         data => {
+            console.log(data)
+            //return a succesfull message upon insertion
+            res.json({ message: 'Ebate created!'});
+        },
         //if there is an error creating, send the error
-        err => res.status(500).json({ error: err});
-    }
+        err => res.status(500).json({ error: err})
+     )
+
 }
